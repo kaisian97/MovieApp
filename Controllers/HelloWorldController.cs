6efec93249690af7c1514f5b3235a9cb5df0d366
MSI- <MSI-@MSI>
2019-06-11 23:28:18 +0800

    Initial Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+     // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+     }
+}
